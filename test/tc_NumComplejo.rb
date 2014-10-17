f83bb42a8cafd851f7155ce3cfc152aaa2c43292
alu0100611120 <alu0100611120@ull.edu.es>
2014-10-17 15:59:33 +0100

    Arreglando error en nombre de variables

--- test/tc_NumComplejo.rb
+++ test/tc_NumComplejo.rb
@@ -28,27 +28,27 @@ class TestNumComplejo < Test::Unit::TestCase
 	def test_suma
 	#Comprobamos la suma con los valores de 'setup'
 	result = @n + @m
-	assert result.real == 4 and result.img == 6
+	assert result.r == 4 and result.i == 6
 	end
 	def test_resta
 	#Comprobamos la resta con los valores de 'setup'
 	result = @n - @m
-	assert result.real == 2 and result.img == 2
+	assert result.r == 2 and result.i == 2
 	end
 	def test_producto
 	#Comprobamos la multiplicacion con los valores de 'setup'
 	result = @n * @m
-	assert result.real == -8 and result.img == 10
+	assert result.r == -8 and result.i == 10
 	end
 	def test_producto_escalar
 	#Comprobamos el producto escalar de un complejo
 	result = @n * @r
-	assert result.real == 6 and result.img == 8
+	assert result.r == 6 and result.i == 8
 	end
 	def test_division
 	#Comprobamos la division con los valores de 'setup'
 	result = @n / @m
-	assert result.real == 11/4 and result.img == -0.5
+	assert result.r == 11/4 and result.i == -0.5
 	end
 end
 
