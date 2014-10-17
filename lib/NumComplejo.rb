67187814011c7678467c8ae307a1bdb3a02de86c
wyllman <wyllman@gmail.com>
2014-10-17 15:13:29 +0100

    Arreglado fallo en el constructor de la libreria de numeros complejos.

--- lib/NumComplejo.rb
+++ lib/NumComplejo.rb
@@ -2,8 +2,8 @@ class NumComplejo
    attr_reader :real, :img
 
    def initialize (nReal, nImg)
-      @real = r.to_f
-      @img  = i.to_f
+      @real = nReal.to_f
+      @img  = nImg.to_f
    end
 
    def to_s
