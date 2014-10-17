cc37fd3012adfd3480d2d0a47d2e7030e387adeb
wyllman <wyllman@gmail.com>
2014-10-17 15:07:22 +0100

    Creadas las operaciones básicas de la libreria de numeros complejos

--- lib/NumComplejo.rb
+++ lib/NumComplejo.rb
@@ -10,4 +10,39 @@ class NumComplejo
       return "(#{real}, #{img})"
    end
 
+   def +(other)
+      if (other.instance_of? NumComplejo)
+         return NumComplejo.new (real + other.real, img + other.img)
+      elsif (other. instance_of Fixnum)
+         return NumComplejo.new (real + other, img)
+      else
+         puts "Error, second argument isn't a valid number."
+      end
+   end 
+
+   def -@
+      return NumComplejo.new (-real, img)
+   end
+
+   def *(other)
+      if (other.instance_of? NumComplejo)
+         return NumComplejo.new (real * other.real - img * other.img,real * other.img + img * other.real)
+      elsif (other.instance_of? Fixnum)
+         return NumComplejo.new ( real * other, img * other)
+      else
+         puts "Error, second argument isn't a valid number."
+      end
+   end
+
+   def /(other)
+      if (other.instance_of? NumComplejo)
+         d = other.real * ohter.real + other.img * other.img
+         return NumComplejo.new ((real * other.real + img * other.img) / d,
+                                (img * other.real - real * other.img) / d)
+      elsif (other.instance_of? Fixnum)
+         return NumComplejo.new (real / other, img / other)
+      else
+         puts "Error, second argument isn't valid number."
+      end
+   end
 end
